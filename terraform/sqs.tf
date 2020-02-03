56a4e8b61b9804a77017428532e263983bad7aaf
John Torakis <j.torakis@thebeat.co>
2020-02-03 11:16:21 +0200

    Changed the SQS to non-fifo
    The PutObject S3 event cannot be subscribed to FIFO queues.
    This event is needed by the 'cloudtrailbeat'

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -2,8 +2,8 @@ resource "aws_sqs_queue" "cloudtrailbeat" {
 
   delay_seconds             = 90
   name                        = "${var.sqs_queue_name}"
-  fifo_queue                  = true
-  content_based_deduplication = true
+  fifo_queue                  = false
+  content_based_deduplication = false
 
   tags = {
     ManagedBy = "Terraform",
